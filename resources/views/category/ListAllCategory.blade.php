04d07e13963e2ec9c092a86648474c5d229e7da7
Gabriel Sales Dorea <133690325+DarkShadow8758@users.noreply.github.com>
2024-12-04 11:13:36 -0300

    refactor: criada nova página de listagem de categorias

--- resources/views/category/ListAllCategory.blade.php
+++ resources/views/category/ListAllCategory.blade.php
@@ -1,11 +1,19 @@
-@extends('category.view_category')
+@push('styles')
+<link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+<link rel="stylesheet" href="{{ asset('css/tables.css') }}">
+@endpush
+@extends('layouts.layout')
 
-@section('title', 'Visualizar Categorias')
-
-@section('header', 'Listar Todas as Categorias') 
+@section('title', 'Página Inicial')
 
 @section('content')
-    <table border="1" style="width: 100%; margin-top: 20px;">
+<div class="container">
+    <table class="girly-table">
         <thead>
             <tr>
                 <th>Título</th>
@@ -13,14 +21,20 @@
             </tr>
         </thead>
         <tbody>
-        @if(isset($categories))
-            @foreach ($categories as $category)
-                <tr>
-                    <td><a href="{{ route('showCategory', $category->id) }}">{{ $category->title }}</a></td>
-                    <td>{{ $category->description }}</td>
-                </tr>
-            @endforeach
+            @if(isset($categories))
+                @foreach ($categories as $category)
+                    <tr>
+                        <td>
+                            <a href="{{ route('showCategory', $category->id) }}">{{ $category->title }}</a>
+                        </td>
+                        <td>{{ $category->description }}</td>
+                    </tr>
+                @endforeach
             @endif
         </tbody>
     </table>
+    <a href="{{ route('CreateCategory') }}" class="btn-create-category">Criar Categoria</a>
+</div>
+
+   
 @endsection
