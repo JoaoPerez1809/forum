9b53c74a69f20af3fbc3922427555762674a102d
Gabriel Sales Dorea <133690325+DarkShadow8758@users.noreply.github.com>
2024-12-08 20:11:36 -0300

    feat: estilizada a pagina de listagem dos usuarios

--- resources/views/users/listAllUsers.blade.php
+++ resources/views/users/listAllUsers.blade.php
@@ -1,14 +1,25 @@
-@extends('layouts.gpt')
+@push('styles')
+<link href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/menu.css') }}">
+<link rel="stylesheet" href="{{ asset('css/tables.css') }}">
+<link rel="stylesheet" href="{{ asset('css/topic.css') }}">
+@endpush
+@extends('layouts.layout')
 
-@section('header', 'Listar Todos os Usuários')
+@section('title', 'Página Inicial')
 
 @section('content')
-
-    <table border="1">
+<div class="container">
+    <table class="girly-table">
         <thead>
             <tr>
                 <th>Nome</th>
                 <th>Email</th>
+                <th>Ações</th>
             </tr>
         </thead>
         <tbody>
@@ -16,9 +27,13 @@
                 <tr>
                     <td>{{ $user->name }}</td>
                     <td>{{ $user->email }}</td>
+                    <td>
+                        <button class="btn-edit">Editar</button>
+                        <button type="submit" class="btn-delete">Deletar</button>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
     </table>
-
+</div>
 @endsection
